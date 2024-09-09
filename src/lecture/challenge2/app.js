7d539ca63d17868a6c0110b51cd9a6d250c99c85
kmaengggong <kmaengggong@gmail.com>
2024-09-09 18:51:31 +0900

    GEO: challenge2

---
+++ src/lecture/challenge2/app.js
@@ -0,0 +1,13 @@
+const calculator = {
+    plus: (a, b) => console.log(a + b),
+    minus: (a, b) => console.log(a - b),
+    multiply: (a, b) => console.log(a * b),
+    divide: (a, b) => console.log(a / b),
+    square: (a, b) => console.log(a ** b)
+}
+
+calculator.plus(1, 2);
+calculator.minus(1, 2);
+calculator.multiply(1, 2);
+calculator.divide(1, 2);
+calculator.square(1, 2);
